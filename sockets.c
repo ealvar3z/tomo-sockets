c48737b26523e10b0ea3ac2b45a0282f561768c3
ealvar3z <55966724+ealvar3z@users.noreply.github.com>
2026-02-03 15:30:18 -0500

    add sockets.c

---
+++ sockets.c
@@ -0,0 +1,23 @@
+#include "sockets.h"
+
+#ifdef _WIN32
+int ts_sock_init(int *out_err) {
+    WSADATA wsa;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        ts_set_err(out_err, WSAGetLastError());
+        return TS_ERR;
+    }
+
+    return TS_OK;
+}
+
+void ts_sock_shutdown(void) { WSACleanup(); }
+#else
+int ts_sock_init(int *out_err) {
+    (void)out_err;
+    return TS_OK;
+}
+
+void ts_sock_shutdown(void) {}
+#endif
